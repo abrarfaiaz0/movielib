164892cfe2bae3f96b8904d51864f9d00a27d78e
abrarfaiazatuno <abrarfaiaz0@gmail.com>
2022-06-12 16:43:08 +0600

    js

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
-let library = []
+
+
+
+let movies = [
+{
+    title : "Mulholland Drive",
+    year : 2001,
+    mlength : "2h 27m",
+    director : "David Lynch",
+    watched : true
+}
+]
+class Movie {
+    constructor(title, year, mlength, director, watched) {
+        this.title = title;
+        this.year = year;
+        this.mlength = mlength;
+        this.director = director;
+        this.watched = watched;
+    }
+}
+
+
+
+let form = document.getElementById("form");
+form.addEventListener("submit", addMovie);
 
 function addMovie(){
-    library.push(Movie)
+    
+    // let title = form.getElementById("title");
+    // let year = form.getElementById("year");
+    // let mlength = form.getElementById("mlength");
+    // let director = form.getElementById("director");
+    // let watched = form.getElementById("watched");
+
+    let title = form.elements[0].value;
+    let year = form.elements[1].value;
+    let mlength = form.elements[2].value;
+    let director = form.elements[3].value;
+    let watched = form.elements[4].value;
+
+    newMovie = new Movie (title,year,mlength,director,watched);
+    movies.push(newMovie);
+    console.log(movies);
 }
 
 
-function Movie(title,year,time,director,watched){
-    this.title = title;
-    this.year = year;
-    this.time = time;
-    this.director = director;
-    this.watched = watched;
-}
 
-let mv = prompt();
-addMovie(mv);
-for(i=0;i<3;i++)
-{console.log(library[0])}
+addMovie(new Movie("vienna",2002,"2h 32m",'yes',false));
+addMovie(new Movie("vienna",2012,"2h 32m",'yes',true));
+
+
